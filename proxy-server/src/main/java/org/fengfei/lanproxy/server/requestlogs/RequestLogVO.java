5c0beaf2e54551af90a29c0afabdabe1435411e7
Sadam·Sadik <1903249375@qq.com>
2024-11-15 17:24:44 +0800

    实现了日志记录页中点击详情Action弹出模态框查看请求包Hex格式和ASCII格式.
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- proxy-server/src/main/java/org/fengfei/lanproxy/server/requestlogs/RequestLogVO.java
+++ proxy-server/src/main/java/org/fengfei/lanproxy/server/requestlogs/RequestLogVO.java
@@ -1,6 +1,7 @@
 package org.fengfei.lanproxy.server.requestlogs;
 
 import java.text.SimpleDateFormat;
+import io.netty.buffer.ByteBuf;
 
 // 添加日志VO类用于页面展示
 public class RequestLogVO {
@@ -11,6 +12,8 @@ public class RequestLogVO {
     private String protocol;
     private long packetSize;
     private String requestInfo;
+    private String hexContent;
+    private String asciiContent;
 
     public RequestLogVO(RequestLog log) {
         this.ip = log.getIp();
@@ -19,6 +22,10 @@ public class RequestLogVO {
         this.requestInfo = log.getRequestInfo();
         this.protocol = log.getProtocol();
         this.packetSize = log.getPacketSize();
+        ByteBuf buf = log.getData();
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.getBytes(buf.readerIndex(), bytes);
+        this.setPacketData(bytes);
     }
 
     // getter方法
@@ -46,4 +53,37 @@ public class RequestLogVO {
         return packetSize;
     }
 
+    public String getHexContent() {
+        return hexContent;
+    }
+
+    public void setHexContent(String hexContent) {
+        this.hexContent = hexContent;
+    }
+
+    public String getAsciiContent() {
+        return asciiContent;
+    }
+
+    public void setAsciiContent(String asciiContent) {
+        this.asciiContent = asciiContent;
+    }
+
+    public void setPacketData(byte[] data) {
+        StringBuilder hexBuilder = new StringBuilder();
+        StringBuilder asciiBuilder = new StringBuilder();
+
+        for (byte b : data) {
+            hexBuilder.append(String.format("%02X ", b));
+
+            if (b >= 32 && b < 127) {
+                asciiBuilder.append((char) b);
+            } else {
+                asciiBuilder.append('.');
+            }
+        }
+
+        this.hexContent = hexBuilder.toString();
+        this.asciiContent = asciiBuilder.toString();
+    }
 }
